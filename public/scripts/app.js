635668ce25a20d036375b3f4a74c1757d8eafef8
Sean Philippi <sean.philippi@gmail.com>
2019-02-25 17:28:19 -0600

    ternary logic in jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,18 +2,23 @@
 
 console.log('App.js is running!');
 
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer'
+};
+
 var template = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    'Indecision App'
+    app.title
   ),
   React.createElement(
     'p',
     null,
-    'This is some info'
+    app.subtitle
   ),
   React.createElement(
     'ol',
@@ -30,6 +35,40 @@ var template = React.createElement(
     )
   )
 );
+var user = {
+  name: 'Andrew',
+  age: '26',
+  location: 'New York'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,15 +1,40 @@
 console.log('App.js is running!');
 
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer'
+};
+
 var template = (
   <div>
-    <h1>Indecision App</h1> 
-    <p>This is some info</p>
+    <h1>{app.title}</h1> 
+    <p>{app.subtitle}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
     </ol>
   </div>
 );
+var user = {
+  name: 'Andrew',
+  age: '26',
+  location: 'New York'
+};
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  } 
+}
+
+var templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+)
+
 var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
