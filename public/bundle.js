db658a34976732564d21936d9639ac734b3b10e9
Sean Philippi <sean.philippi@gmail.com>
2019-04-17 21:23:25 -0500

    ES6 import/export practice

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,64 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-console.log('app.js is running');
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils_js__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__person__ = __webpack_require__(3);
+
+
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(2, 3));
+
+console.log(__WEBPACK_IMPORTED_MODULE_1__person__["c" /* isAdult */](5));
+console.log(__WEBPACK_IMPORTED_MODULE_1__person__["a" /* canDrink */](22));
+console.log(__WEBPACK_IMPORTED_MODULE_1__person__["b" /* default */](64));
+// can import default exports under whatever name we want, but not for regular functions
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["a" /* default */](3, 46));
+
+// person.js
+// named export isAdult(18) - true if adult, else false
+// named export canDrink(18) - true if 21 and over, else false
+
+/***/ }),
+/* 1 */,
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* unused harmony export square */
+/* unused harmony export add */
+console.log('in utils');
+
+const square = x => x * x;
+
+const add = (a, b) => a + b;
+
+// const subtract = 
+// exports must have variable declarations, can be exported one at a time, or destructured like here
+
+/* harmony default export */ __webpack_exports__["a"] = ((a, b) => a - b);
+
+/***/ }),
+/* 3 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+const isAdult = age =>  age >= 18;
+/* harmony export (immutable) */ __webpack_exports__["c"] = isAdult;
+
+
+const canDrink = age => age >= 21;
+/* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
+
+
+// const isSenior = age => age >= 65;
+
+/* harmony default export */ __webpack_exports__["b"] = (age => age >= 65);;
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('app.js is running');
+import defaultThing, { square, add } from './utils.js';
+import goo, { isAdult, canDrink } from './person';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(2, 3));
+
+console.log(isAdult(5));
+console.log(canDrink(22));
+console.log(goo(64));
+// can import default exports under whatever name we want, but not for regular functions
+console.log(defaultThing(3, 46));
+
+// person.js
+// named export isAdult(18) - true if adult, else false
+// named export canDrink(18) - true if 21 and over, else false
