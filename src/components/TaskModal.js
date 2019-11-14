0e5fb75ac00c38e8ed122f3febae6bd1aeca0708
Sean Philippi <sean.philippi@gmail.com>
2019-11-13 22:55:46 -0600

    modal header, body, and task text styling

--- src/components/TaskModal.js
+++ src/components/TaskModal.js
@@ -11,13 +11,13 @@ const TaskModal = props => (
     closeTimeoutMS={ 200 }
     className="modal"
   >
-    <h3>Selected Task</h3>
-    {props.selectedTask && <p>{ props.selectedTask }</p>}
+    <h3 className="modal__title">Selected Task</h3>
+    {props.selectedTask && <p className="modal__body">{ props.selectedTask }</p>}
     <button
       className="button"
       onClick={ () => props.clearTask() }
     >
-      x
+      Okay
     </button>
   </Modal>
 );
